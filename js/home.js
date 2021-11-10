f405b46d1d21a250c8717163c9a89f80d19c44e3
Bomsiwakorn <bomsiwakorns@gmail.com>
2021-11-10 23:00:15 +0700

    Set immage

--- js/home.js
+++ js/home.js
@@ -16,32 +16,32 @@ function animateOverviewNumber() {
   let animateNumberAward = 0;
   if (distance < window.pageYOffset && !viewed) {
     const experienceInterval = setInterval(() => {
-      if (animateNumberExperience < 20) {
+      if (animateNumberExperience < 30) {
         animateNumberExperience += 1;
         yearOfExperienceNumber.innerHTML = animateNumberExperience + '+';
       }
-      if (animateNumberExperience === 20) {
+      if (animateNumberExperience === 30) {
         clearInterval(experienceInterval);
       }
     }, 100);
     const projectInterval = setInterval(() => {
-      if (animateNumberProject < 39) {
-        animateNumberProject += 1;
-        ourProjectsNumber.innerHTML = animateNumberProject;
+      if (animateNumberProject < 200) {
+        animateNumberProject += 10;
+        ourProjectsNumber.innerHTML = animateNumberProject + '+';
       }
-      if (animateNumberProject === 39) {
+      if (animateNumberProject === 200) {
         clearInterval(projectInterval);
       }
-    }, 100);
+    }, 140);
     const awardInterval = setInterval(() => {
-      if (animateNumberAward < 13) {
+      if (animateNumberAward < 18) {
         animateNumberAward += 1;
-        totalAwardsNumber.innerHTML = animateNumberAward;
+        totalAwardsNumber.innerHTML = animateNumberAward + '+';
       }
-      if (animateNumberAward === 13) {
+      if (animateNumberAward === 18) {
         clearInterval(awardInterval);
       }
-    }, 100);
+    }, 170);
     viewed = true;
   }
   // if (window.pageYOffset === 0) {

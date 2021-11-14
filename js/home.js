0abcee99afcea05b2139afe5884c683c2c31c3fe
Bomsiwakorn <bomsiwakorns@gmail.com>
2021-11-14 13:40:54 +0700

    Menu about

--- js/home.js
+++ js/home.js
@@ -1,48 +1,49 @@
 window.onscroll = () => {
-  animateOverviewNumber();
-};
+  animateOverviewNumber()
+  toggleShowNavbar()
+}
 
-let viewed = false;
+let viewed = false
 function animateOverviewNumber() {
   const yearOfExperienceNumber = document.getElementById(
     'YearOfExperienceNumber'
-  );
-  const ourProjectsNumber = document.getElementById('OurProjectsNumber');
-  const totalAwardsNumber = document.getElementById('TotalAwardsNumber');
-  const overviewPage = document.getElementById('OverviewPage');
-  const distance = overviewPage.offsetTop - window.innerHeight;
-  let animateNumberExperience = 0;
-  let animateNumberProject = 0;
-  let animateNumberAward = 0;
+  )
+  const ourProjectsNumber = document.getElementById('OurProjectsNumber')
+  const totalAwardsNumber = document.getElementById('TotalAwardsNumber')
+  const overviewPage = document.getElementById('OverviewPage')
+  const distance = overviewPage.offsetTop - window.innerHeight
+  let animateNumberExperience = 0
+  let animateNumberProject = 0
+  let animateNumberAward = 0
   if (distance < window.pageYOffset && !viewed) {
     const experienceInterval = setInterval(() => {
       if (animateNumberExperience < 30) {
-        animateNumberExperience += 1;
-        yearOfExperienceNumber.innerHTML = animateNumberExperience + '+';
+        animateNumberExperience += 1
+        yearOfExperienceNumber.innerHTML = animateNumberExperience + '+'
       }
       if (animateNumberExperience === 30) {
-        clearInterval(experienceInterval);
+        clearInterval(experienceInterval)
       }
-    }, 100);
+    }, 100)
     const projectInterval = setInterval(() => {
       if (animateNumberProject < 200) {
-        animateNumberProject += 10;
-        ourProjectsNumber.innerHTML = animateNumberProject + '+';
+        animateNumberProject += 10
+        ourProjectsNumber.innerHTML = animateNumberProject + '+'
       }
       if (animateNumberProject === 200) {
-        clearInterval(projectInterval);
+        clearInterval(projectInterval)
       }
-    }, 140);
+    }, 140)
     const awardInterval = setInterval(() => {
       if (animateNumberAward < 18) {
-        animateNumberAward += 1;
-        totalAwardsNumber.innerHTML = animateNumberAward + '+';
+        animateNumberAward += 1
+        totalAwardsNumber.innerHTML = animateNumberAward + '+'
       }
       if (animateNumberAward === 18) {
-        clearInterval(awardInterval);
+        clearInterval(awardInterval)
       }
-    }, 170);
-    viewed = true;
+    }, 170)
+    viewed = true
   }
   // if (window.pageYOffset === 0) {
   //   viewed = false;
@@ -50,4 +51,30 @@ function animateOverviewNumber() {
   //   animateNumberProject = 0;
   //   animateNumberAward = 0;
   // }
+}
+
+function toggleShowNavbar() {
+  const navbar = document.getElementById('NavbarPage')
+  if (window.pageYOffset > 0) {
+    navbar.style.background = 'rgba(0, 0, 0, 0.4)'
+  } else {
+    navbar.style.background = 'none'
+  }
+}
+
+let isShowAboutMenu = false
+function toggleSubMenu() {
+  const aboutMenu = document.querySelector('.navbar-menu-sub-items')
+  const iconArrowAbout = document.querySelector('.nav-menu-all li div i')
+  if (isShowAboutMenu) {
+    aboutMenu.style.setProperty('padding', '0', 'important')
+    aboutMenu.style.height = '0'
+    iconArrowAbout.style.transform = 'rotate(0deg)'
+    isShowAboutMenu = false
+  } else {
+    aboutMenu.style.height = '128px'
+    aboutMenu.style.setProperty('padding', '1rem', 'important')
+    iconArrowAbout.style.transform = 'rotate(180deg)'
+    isShowAboutMenu = true
+  }
 }

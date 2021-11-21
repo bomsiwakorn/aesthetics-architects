695483dc5bc15bafa03edeec60d946100dd08f46
Bomsiwakorn <bomsiwakorns@gmail.com>
2021-11-22 00:50:04 +0700

    Animate

--- js/about.js
+++ js/about.js
@@ -1,3 +1,23 @@
+const sectionAllImages = document.querySelectorAll(
+  '[class^="team-profile-img"]'
+)
+const sectionImagesObserver = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    const windowBgSlide = document.querySelectorAll('.window-bg-slide')
+    const className = entry.target.className
+    const indexImg = className.substring(className.length - 1)
+    if (entry.isIntersecting) {
+      windowBgSlide[indexImg].style.width = '0'
+    }
+    // else {
+    //   windowBgSlide[indexImg].style.width = '250px'
+    // }
+  })
+})
+for (let i = 0; i < sectionAllImages.length; i++) {
+  sectionImagesObserver.observe(sectionAllImages[i])
+}
+
 const imageProfileDefault = {
   0: './asset/about/team/Nadadhorn 1.jpg',
   1: './asset/about/team/Veerachat 2.jpg',
@@ -15,21 +35,17 @@ const imageProfileColor = {
 }
 
 function handleHoverImageProfile(index) {
-  const profile = document.querySelectorAll('.team-profile-img')
-  profile[index].children[0].style.opacity = '0'
-  setTimeout(() => {
-    profile[index].children[0].src = imageProfileColor[index]
-    profile[index].children[0].style.opacity = '1'
-  }, 100)
+  const profileImgColor = document.querySelectorAll('.profile-img-color')
+  if (profileImgColor[index]) {
+    profileImgColor[index].style.opacity = '1'
+  }
 }
 
 function handleUnHoverImageProfile(index) {
-  const profile = document.querySelectorAll('.team-profile-img')
-  profile[index].children[0].style.opacity = '0'
-  setTimeout(() => {
-    profile[index].children[0].src = imageProfileDefault[index]
-    profile[index].children[0].style.opacity = '1'
-  }, 100)
+  const profileImgColor = document.querySelectorAll('.profile-img-color')
+  if (profileImgColor[index]) {
+    profileImgColor[index].style.opacity = '0'
+  }
 }
 
 const imagesDefault1 = {
@@ -50,32 +66,32 @@ const imagesColor1 = {
   5: './asset/about/logo/the-ritz-carlton--eps--vector-logo-colour.svg',
 }
 
-const text1 = {
-  0: {
-    name: 'Mandarin Oriental Hotels Group',
-    info: 'For Mandarin Oriental DharaDhevi Chiangmai (2004)',
-  },
-  1: {
-    name: 'Accor Hotels Group',
-    info: 'For Ibis Pattaya , Ibis Phuket , Ibis Bangkok (2005)',
-  },
-  2: {
-    name: 'JW Marriott Hotels Group',
-    info: 'For Marriott Samui (2010)',
-  },
-  3: {
-    name: 'Hilton Hotels Group',
-    info: 'For Conrad Samui (2009)',
-  },
-  4: {
-    name: 'Intercontinental Hotels Group',
-    info: 'For Intercontinental Resort Samui (2009)',
-  },
-  5: {
-    name: 'The Ritz-Carlton Hotel Company',
-    info: 'For The Ritz-Carlton at Royal Rajhadumri',
-  },
-}
+// const text1 = {
+//   0: {
+//     name: 'Mandarin Oriental Hotels Group',
+//     info: 'For Mandarin Oriental DharaDhevi Chiangmai (2004)',
+//   },
+//   1: {
+//     name: 'Accor Hotels Group',
+//     info: 'For Ibis Pattaya , Ibis Phuket , Ibis Bangkok (2005)',
+//   },
+//   2: {
+//     name: 'JW Marriott Hotels Group',
+//     info: 'For Marriott Samui (2010)',
+//   },
+//   3: {
+//     name: 'Hilton Hotels Group',
+//     info: 'For Conrad Samui (2009)',
+//   },
+//   4: {
+//     name: 'Intercontinental Hotels Group',
+//     info: 'For Intercontinental Resort Samui (2009)',
+//   },
+//   5: {
+//     name: 'The Ritz-Carlton Hotel Company',
+//     info: 'For The Ritz-Carlton at Royal Rajhadumri',
+//   },
+// }
 
 let experiences1SelectedIndex = 0
 let experiences1PrevSelectIndex = 0
@@ -83,21 +99,12 @@ let experiences1PrevSelectIndex = 0
 selectExperiences1(experiences1SelectedIndex)
 function selectExperiences1(index) {
   experiences1PrevSelectIndex = experiences1SelectedIndex
-
-  const experiencesName1 = document.getElementById('ExperiencesName1')
-  experiencesName1.innerText = text1[index].name
-
-  const experiencesInfo1 = document.getElementById('ExperiencesInfo1')
-  experiencesInfo1.innerText = text1[index].info
-
-  if (window.innerWidth <= 991) {
-    const experienceTextWrapper = document.querySelectorAll(
-      '.experiences-text-wrapper-m'
-    )
-    experienceTextWrapper[index].style.display = 'block'
-    if (experiences1PrevSelectIndex !== index) {
-      experienceTextWrapper[experiences1PrevSelectIndex].style.display = 'none'
-    }
+  const experienceTextWrapper = document.querySelectorAll(
+    '[class^="experiences-text-wrapper-m"]'
+  )
+  experienceTextWrapper[index].style.display = 'grid'
+  if (experiences1PrevSelectIndex !== index) {
+    experienceTextWrapper[experiences1PrevSelectIndex].style.display = 'none'
   }
 
   const experiences1 = document.querySelectorAll('.experiences1-item')
@@ -150,93 +157,6 @@ const imagesColor2 = {
   5: './asset/about/logo/REIGNWOOD - Logo 1.svg',
 }
 
-const text2 = {
-  0: [
-    {
-      name: 'I condo Sukhumvit 103',
-      info: 'For Property Perfect PCL.(2010)',
-    },
-    {
-      name: 'Metro Sky Kaset',
-      info: 'For Property Perfect PCL.(2011)',
-    },
-    {
-      name: 'Metro Sky Prachachuen',
-      info: 'For Property Perfect PCL.(2012)',
-    },
-    {
-      name: 'Metro Sky Rama 4',
-      info: 'For Property Perfect PCL.(2013)',
-    },
-  ],
-  1: [
-    {
-      name: 'The Crest Sukhumvit 24',
-      info: 'For SC Asset Corporation PCL.(2011)',
-    },
-  ],
-  2: [
-    {
-      name: 'Setthasiri Onnut-Srinakarindra',
-      info: 'For San siri PCL.(2011)',
-    },
-    {
-      name: 'Narasiri By Sansiri',
-      info: 'For Sansiri PCL.(2012)',
-    },
-  ],
-  3: [
-    {
-      name: 'The Connect By Pruksa',
-      info: 'For Pruksa Real Estate PCL.(2009)',
-    },
-    {
-      name: 'Pruksa Ville By Pruksa',
-      info: 'For Pruksa Real Estate PCL.(2009)',
-    },
-  ],
-  4: [
-    {
-      name: 'Atoll Java Bay',
-      info: 'Ananda Development PCL.(2013)',
-    },
-  ],
-  5: [
-    {
-      name: 'Wentworth Golf Club',
-      info: '',
-    },
-    {
-      name: 'Wentwort Driving Range',
-      info: '',
-    },
-    {
-      name: 'Kis international school Lumlookka',
-      info: '',
-    },
-    {
-      name: 'Reignwood Sport Club',
-      info: '',
-    },
-    {
-      name: 'Reignwood retirement Center',
-      info: '',
-    },
-    {
-      name: 'Reignwood Estate',
-      info: '',
-    },
-    {
-      name: 'Reignwood Golf Estate',
-      info: '',
-    },
-    {
-      name: 'Private Residence',
-      info: '',
-    },
-  ],
-}
-
 let experiences2SelectedIndex = 0
 let experiences2PrevSelectIndex = 0
 
@@ -244,24 +164,24 @@ selectExperiences2(experiences2SelectedIndex)
 function selectExperiences2(index) {
   experiences2PrevSelectIndex = experiences2SelectedIndex
 
-  const experiencesName2 = document.getElementById('ExperiencesName2')
-  experiencesName2.innerHTML = ''
-  text2[index].forEach((text, i) => {
-    experiencesName2.innerHTML += `
-      <li class="text-experience2-name">${text.name}</li>
-      <div class="text-experience2-info">${text.info}</div>
-    `
-  })
+  // const experiencesName2 = document.getElementById('ExperiencesName2')
+  // experiencesName2.innerHTML = ''
+  // text2[index].forEach((text, i) => {
+  //   experiencesName2.innerHTML += `
+  //     <li class="text-experience2-name">${text.name}</li>
+  //     <div class="text-experience2-info">${text.info}</div>
+  //   `
+  // })
 
-  if (window.innerWidth <= 991) {
-    const experienceTextWrapper = document.querySelectorAll(
-      '.experiences2-text-wrapper-m'
-    )
-    experienceTextWrapper[index].style.display = 'block'
-    if (experiences2PrevSelectIndex !== index) {
-      experienceTextWrapper[experiences2PrevSelectIndex].style.display = 'none'
-    }
+  // if (window.innerWidth <= 991) {
+  const experienceTextWrapper = document.querySelectorAll(
+    '[class^="experiences2-text-wrapper-m"]'
+  )
+  experienceTextWrapper[index].style.display = 'grid'
+  if (experiences2PrevSelectIndex !== index) {
+    experienceTextWrapper[experiences2PrevSelectIndex].style.display = 'none'
   }
+  // }
 
   const experiences2 = document.querySelectorAll('.experiences2-item')
 

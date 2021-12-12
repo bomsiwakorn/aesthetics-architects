c6cb9393c7721a68855d9d8da2a4e46b83433dcf
Bomsiwakorn <bomsiwakorns@gmail.com>
2021-12-12 20:34:03 +0700

    Edit

--- public/js/home.js
+++ public/js/home.js
@@ -94,12 +94,14 @@ function openSidebar() {
 }
 
 function closeSidebar() {
-  const sidebar = document.querySelector('.nav-menu-all')
-  const closeIcon = document.querySelector('.icon-close-sidebar')
-  sidebar.style.left = '800px'
-  sidebar.style.opacity = '0'
-  closeIcon.style.display = 'none'
-  document.querySelector('html').style.overflow = 'auto'
+  if (window.innerWidth <= 768) {
+    const sidebar = document.querySelector('.nav-menu-all')
+    const closeIcon = document.querySelector('.icon-close-sidebar')
+    sidebar.style.left = '800px'
+    sidebar.style.opacity = '0'
+    closeIcon.style.display = 'none'
+    document.querySelector('html').style.overflow = 'auto'
+  }
 }
 
 let isShowAboutMenu = false
@@ -123,7 +125,7 @@ function toggleSubMenu() {
     }
     isShowAboutMenu = false
   } else {
-    aboutMenu.style.height = '128px'
+    aboutMenu.style.height = '94px'
     aboutMenu.style.setProperty('padding', '1rem', 'important')
     if (window.innerWidth <= 768) {
       iconArrowAboutM.style.transform = 'rotate(180deg)'
